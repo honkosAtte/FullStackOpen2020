37c81cd2c6ab606286b8047da01f2d0467043bfd
Atte Honkonen <atte.honkonen@gmail.com>
2020-10-22 15:28:19 +0300

    1.4

--- 1/kurssitiedot/src/index.js
+++ 1/kurssitiedot/src/index.js
@@ -4,18 +4,20 @@ import ReactDOM from 'react-dom'
 const App = () => {
 
   const course = 'Half Stack application development'
-  const part1 = {
-    name: 'Fundamentals of React',
-    exercises: 10
-  }
-  const part2 = {
-    name: 'Using props to pass data',
-    exercises: 7
-  }
-  const part3 = {
-    name: 'State of a component',
-    exercises: 14
-  }
+  const parts = [
+    {
+      name: 'Fundamentals of React',
+      exercises: 10
+    },
+    {
+      name: 'Using props to pass data',
+      exercises: 7
+    },
+    {
+      name: 'State of a component',
+      exercises: 14
+    }
+  ]
 
   const Header = ({course}) => {
     return (
@@ -49,8 +51,8 @@ const App = () => {
   return (
     <div>
       <Header course={course}/>
-      <Content list={[part1, part2, part3]}/>
-      <Total total={part1.exercises + part2.exercises + part3.exercises}/>
+      <Content list={parts}/>
+      <Total total={parts[0].exercises + parts[1].exercises + parts[2].exercises}/>
     </div>
   )
 }
