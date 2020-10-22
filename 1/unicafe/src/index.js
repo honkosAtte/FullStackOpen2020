ad9b783edae4ab8273f4cc5101f8d0631c5fced6
Atte Honkonen <atte.honkonen@gmail.com>
2020-10-22 19:58:10 +0300

    1.7

--- 1/unicafe/src/index.js
+++ 1/unicafe/src/index.js
@@ -6,10 +6,25 @@ const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  const [all, setAll] = useState(0)
+  const [positive, setPositive] = useState(0)
+  const [average, setAverage] = useState(0)
 
-  const handleGood = () => setGood(good + 1);
-const handleNeutral = () => setNeutral(neutral +1);
-const handleBad = () => setBad(bad + 1);
+  const handleGood = () => {setGood(good + 1)
+  setAll(all+1)
+  setPositive(positive+1)
+  setAverage(average+1)
+};
+const handleNeutral = () => {setNeutral(neutral +1)
+  setAll(all+1)
+  setPositive(positive+0)
+  setAverage(average+0)
+};
+const handleBad = () => {setBad(bad + 1)
+setAll(all+1) 
+setAverage(average-1)
+};;
+
 
   return (
     <div>
@@ -21,10 +36,13 @@ const handleBad = () => setBad(bad + 1);
 
 <h1>stats</h1>
 
-good {good}, 
-neutral {neutral}, 
-bad {bad}
-      
+good {good} <br/>
+neutral {neutral} <br/>
+bad {bad} <br/>
+all {all} <br/>
+average {average? average/all : 0} <br/>
+positive {positive? positive/all * 100: 0} %
+
     </div>
   )
 }
