bd5a787805c7822f2cecf1a3ea92db2b37ccc880
Atte Honkonen <atte.honkonen@gmail.com>
2020-10-22 19:19:10 +0300

    1.6

---
+++ 1/unicafe/src/index.js
@@ -0,0 +1,36 @@
+import React, { useState } from 'react'
+import ReactDOM from 'react-dom'
+
+const App = () => {
+  // tallenna napit omaan tilaansa
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  const handleGood = () => setGood(good + 1);
+const handleNeutral = () => setNeutral(neutral +1);
+const handleBad = () => setBad(bad + 1);
+
+  return (
+    <div>
+      
+      <h1>give feedback</h1>
+<button onClick={handleGood}>good</button>
+<button onClick={handleNeutral}>neutral</button>
+<button onClick={handleBad}>bad</button>
+
+<h1>stats</h1>
+
+good {good}, 
+neutral {neutral}, 
+bad {bad}
+      
+    </div>
+  )
+}
+
+ReactDOM.render(<App />, 
+  document.getElementById('root')
+)
+
+
