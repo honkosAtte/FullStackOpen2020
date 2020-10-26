85fb8179431e2e387522e79d78143c29653aa7d6
Atte Honkonen <atte.honkonen@gmail.com>
2020-10-26 20:11:50 +0200

    2.14

--- 2/maidentiedot/src/App.js
+++ 2/maidentiedot/src/App.js
@@ -1,19 +1,32 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios'
 
-const OneItemButton = ({stateOfTheRow, setOneState}) => (
-  <button onClick={() => setOneState( {
+const apiKey = process.env.REACT_APP_API_KEY
+const WeatherApiBaseUrl = 'http://api.weatherstack.com/current?access_key=' + apiKey + '&query='
+
+
+const OneItemButton = ({stateOfTheRow, setOneState, weatherDataForCapital, setWeatherDataForCapital}) => (
+  <button onClick={() => 
+    {
+    const weatherInfo = axios.get(WeatherApiBaseUrl+stateOfTheRow.capital)
+    .then(response => {
+    console.log(response)
+
+    setWeatherDataForCapital(response.data.success !== false ? response.data.current.temperature : 'no data')})
+
+    setOneState( {
     name: stateOfTheRow.name,
     capital: stateOfTheRow.capital,
     population: stateOfTheRow.population,
     languages: stateOfTheRow.languages.map(lang => lang.name),
     imageUrl: stateOfTheRow.flag
-    })
+    })}
     
   }>Show details</button>
 )
 
-const OneItem = ({oneState}) => (
+const OneItem = ({oneState, weatherDataForCapital}) => {
+  return(
 <>
   <h1>{oneState.name}</h1>
   
@@ -26,7 +39,8 @@ const OneItem = ({oneState}) => (
   
   <img src={oneState.imageUrl} height='220px'/>
   
-  </>)
+  {weatherDataForCapital!== 'no data' ? <p>temperature in {oneState.capital}: {weatherDataForCapital} Celsius</p> : <p></p>}
+  </>)}
 
 const App = () => {
  const [states, setStates] = useState([])
@@ -38,6 +52,7 @@ const [oneState, setOneState] = useState({ name: '',
   languages: ['', ''],
   imageUrl: ''})
 
+  const [weatherDataForCapital, setWeatherDataForCapital] = useState()
 
 
  const handleInputChange = (e) => {
@@ -48,25 +63,25 @@ const [oneState, setOneState] = useState({ name: '',
   if (filtered && filtered.length > 0)
   setFilteredStates(filtered)
 
-  if (filtered.length === 1)
-  {
-    const obj = filtered[0]
-    console.log('obj', obj)
-    const lang = obj.languages.map(lang => lang.name)
-    console.log('lang', lang)
+//   if (filtered.length === 1)
+//   {
+//     const obj = filtered[0]
+//     console.log('obj', obj)
+//     const lang = obj.languages.map(lang => lang.name)
+//     console.log('lang', lang)
 
-  setOneState(
-    {
-      name: obj.name,
-      capital: obj.capital,
-      population: obj.population,
-      languages: lang,
-      imageUrl: obj.flag
-      }
+//   setOneState(
+//     {
+//       name: obj.name,
+//       capital: obj.capital,
+//       population: obj.population,
+//       languages: lang,
+//       imageUrl: obj.flag
+//       }
     
-    )
- }
-  console.log('onState', oneState)
+//     )
+//  }
+//   console.log('onState', oneState)
 }
 
  useEffect(() => {
@@ -82,14 +97,14 @@ const [oneState, setOneState] = useState({ name: '',
     <div>
       <input placeholder='search' value={filterInputState} onChange={handleInputChange}/>
       <br/>
-   {/* <ul>{states.map((row) => <p key={row.name}>{row.name}</p>)}</ul> */}
+
 {filterInputState.length > 0 
 && filteredStates.length > 1 
 && filteredStates.length < 11 ? 
-filteredStates.map((row) => <> <li key={row.name}>{row.name} <OneItemButton stateOfTheRow={row} setOneState={setOneState}/></li> </>) : <p/>}
+filteredStates.map((row) => <> <li key={row.name}>{row.name} <OneItemButton stateOfTheRow={row} setOneState={setOneState} weatherDataForCapital={weatherDataForCapital} setWeatherDataForCapital={setWeatherDataForCapital}/></li> </>) : <p/>}
 {filterInputState.length > 0 && filteredStates.length > 10 ? 'Too many matches, please specify': <p></p>}
 
-{oneState.name !== '' ? <OneItem oneState={oneState}/> : <p></p>}
+{oneState.name !== '' ? <OneItem oneState={oneState} weatherDataForCapital={weatherDataForCapital}/> : <p></p>}
 
 
 
